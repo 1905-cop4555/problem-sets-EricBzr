dbf9db6c6c0432c17012504ceb2fd1be0eda9ee9
Eric Bazan <ebazan7029@gmail.com>
2020-01-15 01:03:14 -0500

    Created two new modules that test simple built-in pair functions and contain a recursive function respectively.

--- LearningFSharp/LearningFSharp/Code.fs
+++ LearningFSharp/LearningFSharp/Code.fs
@@ -5,3 +5,23 @@ module Exercise1 =
    let increment n = n + 1
 
    let decrement n = n - 1
+
+module Exercise2 =
+
+   let str1 = "cat"
+
+   let str2 = "catdog"
+
+   let letter =
+      printfn "%c" "ball".[0]
+   
+   let printPairs = 
+      printfn "%s" (fst("penguin", "lizard"))
+      printfn "%s" (snd("penguin", "lizard"))
+
+module Exercise3 = 
+
+   let rec power pair = 
+      if snd pair = 0
+        then 1.0
+      else fst pair * power (fst pair, (snd pair) - 1)
--- LearningFSharp/LearningFSharp/Program.fs
+++ LearningFSharp/LearningFSharp/Program.fs
@@ -8,6 +8,9 @@ let succ n = n + 1
 let main argv =
     printfn "---------- Exercise One ----------"
     printfn "%d" (Code.Exercise1.increment 11)
+
+    printfn "---------- Exercise Two ----------"
+    printfn "%f" (Code.Exercise3.power (2.0, 4))
      
     printfn "\nHello World from F#!"
     0 // return an integer exit code
